8a55b98441b0812179274a70184c1707f6e14ce4
Zhe Lu <lzanita09@gmail.com>
2014-05-03 20:24:19 -0400

    Added Settings.

---
+++ src/main/java/com/reindeercrafts/notificationpeek/settings/PreferenceKeys.java
@@ -0,0 +1,12 @@
+package com.reindeercrafts.notificationpeek.settings;
+
+/**
+ * Static fields for preference keys
+ *
+ * Created by zhelu on 5/3/14.
+ */
+public class PreferenceKeys {
+
+    public static final String PREF_CLOCK = "show_clock";
+    public static final String PREF_PEEK_TIMEOUT = "peek_time_out";
+}
